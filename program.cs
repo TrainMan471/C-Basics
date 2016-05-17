feb8e082902b9cc1088d162baa8bb3ff1d6be042
Michael Brown <Michael Brown>
2016-05-17 13:35:05 -0400

    Cleaned up the code in program.cs

--- program.cs
+++ program.cs
@@ -1,25 +1,24 @@
-class Program
-{
-static void Main()
-{
- //Prompt the user for minutes exercises
+using System;
 
- System.Console.Write("Enter How Many you Exercised");
+  namespace TreeHouse.FitnessFrog
+  {
+          class Program
+              {
+                  static void Main()
+                    {
+                        //Prompt the user for minutes exercises
+                          Console.Write("Enter How Many you Exercised");
 
-string entry = System.Console.ReadLine();
+                          string entry = System.Console.ReadLine();
 
+                          string firstName = "Michael";    }//end static void Main()
 
+                        }//End the class
 
-
-string firstName = "Michael";
+  }//End the namespace/Class
 
 
  //Add minutes exercised to total
 
  //Display total minutes to the screen
- //Repeat until the user quits
-
-}
-
-
-}
+ //Repeat until the user quiits
