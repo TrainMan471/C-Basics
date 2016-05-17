df97c778f0b02cdd37fecbcf10622de3b9e20f92
Michael Brown <Michael Brown>
2016-05-17 13:22:13 -0400

    Notes on TreeHouse from C#

---
+++ program.cs
@@ -0,0 +1,25 @@
+class Program
+{
+static void Main()
+{
+ //Prompt the user for minutes exercises
+
+ System.Console.Write("Enter How Many you Exercised");
+
+string entry = System.Console.ReadLine();
+
+
+
+
+string firstName = "Michael";
+
+
+ //Add minutes exercised to total
+
+ //Display total minutes to the screen
+ //Repeat until the user quits
+
+}
+
+
+}
